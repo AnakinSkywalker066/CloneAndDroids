c3935b182050bdb9090aa2564ea8247a2378bc53
AnakinSkywalker066 <Leegovols@gmail.com>
2023-05-16 13:14:43 -0400

    Initial commit

---
+++ CloneWars.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using BTD_Mod_Helper;
+using CloneWars;
+
+[assembly: MelonInfo(typeof(CloneWars.CloneWars), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace CloneWars;
+
+public class CloneWars : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<CloneWars>("Clone Wars Mod Has loaded!");
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace CloneWars;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "36.3";
+    public const string Version = "1.0";
+    public const string Name = "CloneWars";
+
+    public const string Description = "Add Clones And Droids To BTD6";
+
+    public const string RepoOwner = "AnakinSkywalker066"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "CloneAndDroids"; // TODO add your repo name here, also in the download url in README.md
+}
