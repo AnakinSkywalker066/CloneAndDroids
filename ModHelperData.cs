f93ebe89c2f1f5def6539e01f0b6f10bce5e13d0
Edward Frye <leegovols@gmail.com>
2023-06-04 16:58:06 -0400

    2.0 Updates!

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "2.0";
     public const string Name = "Clone Wars";
 
-    public const string Description = "Add Clones And Droids To BTD6";
+    public const string Description = "Add Clones TO BTD 6!";
 
     public const string RepoOwner = "AnakinSkywalker066"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "CloneAndDroids"; // TODO add your repo name here, also in the download url in README.md
